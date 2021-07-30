f1c5318d964152e372cf7587cd97d04f732e0631
Igor Markiv <Igor.vikram@gmail.com>
2021-07-30 15:41:51 +0200

    Init

---
+++ DItest/DItest/Managers/AuthManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DItest.Managers
+{
+	public class AuthManager
+	{
+        public interface IAuthManager
+        {
+            Task<Microsoft.AspNetCore.Identity.IdentityUser> FindByIdAsync(string id); // Get user id:
+        }
+
+        //public class testdI : IAuthManager
+        //{
+        //	// здесь должна быть импементация FindByIdAsync(string id) из UserManager<Data.CustomIdentityUser> но впихнуть не впихуемое не получается
+        //	public Task<CustomIdentityUser> FindByIdAsync(string id)
+        //	{
+        //		throw new NotImplementedException();
+        //	}
+        //}
+
+        public class AuthManager : IAuthManager
+        {
+            private IAuthManager _iauthManager;
+
+            public AuthManager(IAuthManager iauthManager)
+            {
+                this._iauthManager = iauthManager; //тут мы получаем налл
+            }
+
+            public Task<Microsoft.AspNetCore.Identity.IdentityUser> FindByIdAsync(string id)
+            {
+                var result = _iauthManager.FindByIdAsync(id);
+                return result;
+            }
+        }
+    }
+}
---
+++ DItest/DItest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DItest.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
